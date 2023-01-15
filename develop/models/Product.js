a61ccb11ec76f80072cfb6c8035db7886745b87b
braden wales <bra.wales00@gmail.com>
2023-01-15 11:33:06 -0600

    added fields to product.js

--- develop/models/Product.js
+++ develop/models/Product.js
@@ -10,6 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER ,
+      allowNull: false ,
+      primaryKey: true ,
+      autoIncrement: true ,
+    } ,
+    product_name: {
+      type: DataTypes.STRING ,
+      allowNull: false ,
+    },
+    stock: {
+      type: DataTypes.INTEGER ,
+      allowNull: false ,
+      defaultValue: 10 ,
+      validate: {
+        inNumeric: true
+      }
+    } ,
+    price: {
+      type: DataTypes.INTEGER , 
+      allowNull: false ,
+      validate: {
+        inDecimal: true
+      }
+    } ,
+    category_id: {
+      type: DataTypes.INTEGER ,
+      references: {
+        model: 'category' ,
+        key: 'id' ,
+      } ,
+    } ,
   },
   {
     sequelize,
